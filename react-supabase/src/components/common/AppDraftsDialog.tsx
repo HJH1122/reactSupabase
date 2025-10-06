1dc5fc3a1ac48d2167dbb25b87965641aa11afc8
hjh <ktorn21@naver.com>
2025-10-06 22:05:56 +0900

    임시저장 기능 추가

--- react-supabase/src/components/common/AppDraftsDialog.tsx
+++ react-supabase/src/components/common/AppDraftsDialog.tsx
@@ -10,7 +10,14 @@ import {
   DialogTitle,
   DialogTrigger,
 } from "@/components/ui"
+import supabase from "@/lib/supabase";
+import { useAuthStore } from "@/stores";
 import { Separator } from "@radix-ui/react-separator";
+import { useEffect, useState } from "react";
+import { toast } from "sonner";
+import dayjs from 'dayjs'
+import { TOPIC_STATUS, type Topic } from "@/types/topic.type";
+import { useNavigate } from "react-router";
 
 interface Props {
     children: React.ReactNode;
@@ -18,43 +25,85 @@ interface Props {
 
 
 export function AppDraftsDialog({children}: Props) {
-  return (
-    <Dialog>
-        <DialogTrigger asChild>{children}</DialogTrigger>
-        <DialogContent>
-            <DialogHeader>
-                <DialogTitle>임시 저장된 토픽</DialogTitle>
-                <DialogDescription>임시 저장된 토픽 목록입니다. 이어서 작성하거나 삭제할 수 있습니다.</DialogDescription>
-            </DialogHeader>
-            <div className="grid gap-3 py-4">
-                <div className="flex items-center gap-2">
-                    <p>임시 저장</p>
-                    <p className="text-base text-green-600 -mr-[6px]">10</p>
-                    <p>건</p>
-                </div>
-                <Separator />
-                <div className="min-h-60 flex items-center justify-center">
-                    <p className="text-muted-foreground/50">조회 가능한 정보가 없습니다.</p>
-                </div>
-                <div className="min-h-60 flex items-center justify-center gap-3">
-                    <div className="w-full flex items-center justify-between py-2 px-4 rounded-md bg-card/50 cursor-pointer">
-                        <div className="flex items-start gap-2">
-                            <Badge className="w-5 h-5 mt-[3px] rounded-sm aspect-square text-foreground bg-[#E26F24] hover:bg-[#E26F24]">1</Badge>
-                            <div className="flex flex-col">
-                                <p>등록된 제목이 없습니다.</p>
-                                <p className="text-xs text-muted-foreground">작성일: 2025. 09. 12.</p>
-                            </div>
-                        </div>
-                        <Badge variant={'outline'}></Badge>
+    const user = useAuthStore((state) => state.user)
+    const navigate = useNavigate();
+    const [drafts, setDrafts] = useState<any[]>([]);
+
+    const fetchDrafts = async() =>{
+        try{
+            const { data: topics, error } = await supabase
+                .from('topic')
+                .select("*")
+                .eq('author', user.id)
+                .eq('status', TOPIC_STATUS.TEMP);
+
+            if(error){
+                toast.error(error.message);
+                return;
+            }
+            if(topics){
+                setDrafts(topics);
+            }
+        }catch(error){
+
+
+        }
+    }
+
+    useEffect(()=>{
+
+        if(user.email) fetchDrafts();
+    }, []);
+
+    return (
+        <Dialog>
+            <DialogTrigger asChild>{children}</DialogTrigger>
+            <DialogContent>
+                <DialogHeader>
+                    <DialogTitle>임시 저장된 토픽</DialogTitle>
+                    <DialogDescription>임시 저장된 토픽 목록입니다. 이어서 작성하거나 삭제할 수 있습니다.</DialogDescription>
+                </DialogHeader>
+                <div className="grid gap-3 py-4">
+                    <div className="flex items-center gap-2">
+                        <p>임시 저장</p>
+                        <p className="text-base text-green-600 -mr-[6px]">{drafts.length}</p>
+                        <p>건</p>
                     </div>
+                    <Separator />
+                    {drafts.length > 0 ? (
+                    <div className="min-h-60 h-60 flex flex-col items-center justify-start gap-3 overflow-y-scroll">
+                        {
+                            drafts.map((draft: Topic, index: number) =>{
+                            return (
+                                    <div className="w-full flex items-center justify-between py-2 px-4 rounded-md bg-card/50 cursor-pointer" onClick={() => navigate(`/topics/${draft.id}/create`)}>
+                                        <div className="flex items-start gap-2">
+                                            <Badge className="w-5 h-5 mt-[3px] rounded-sm aspect-square text-foreground bg-[#E26F24] hover:bg-[#E26F24]">{index + 1}</Badge>
+                                            <div className="flex flex-col">
+                                                <p>{draft.title}</p>
+                                                <p className="text-xs text-muted-foreground">작성일: {dayjs(draft.created_at).format('YYYY. MM. DD')}</p>
+                                            </div>
+                                        </div>
+                                        <Badge variant={'outline'}>작성중</Badge>
+                                    </div>
+                                
+                                
+                                );
+                            })
+                        }
+                        </div>
+                        ) : (
+                            <div className="min-h-60 flex items-center justify-center">
+                                <p className="text-muted-foreground/50">조회 가능한 정보가 없습니다.</p>
+                            </div>
+                            )}
+
                 </div>
-            </div>
-            <DialogFooter>
-                <DialogClose asChild>
-                    <Button type="button" variant={'outline'} className="border-0">닫기</Button>
-                </DialogClose>
-            </DialogFooter>
-        </DialogContent>
-    </Dialog>
-  )
+                <DialogFooter>
+                    <DialogClose asChild>
+                        <Button type="button" variant={'outline'} className="border-0">닫기</Button>
+                    </DialogClose>
+                </DialogFooter>
+            </DialogContent>
+        </Dialog>
+    )
 }
--- react-supabase/src/pages/index.tsx
+++ react-supabase/src/pages/index.tsx
@@ -22,7 +22,7 @@ function App() {
       .from('topic')
       .insert([
         {
-          status: 'temp',
+          status: null,
           title: null,
           content: null,
           category: null,
